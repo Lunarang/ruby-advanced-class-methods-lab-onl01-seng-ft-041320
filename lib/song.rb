9b2c25506bc86e43aa0e163e1ef2163fc57c5cdd
Erin Mettille <ecmettille@gmail.com>
2020-04-24 23:31:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,9 @@ class Song
     song
   end
   
-  def self.new_by_name(song)
+  def self.new_by_name(name)
     song = Song.new 
-    song.name = name 
+    song.name = name
   end
     
   def self.creat_by_name(name) 
